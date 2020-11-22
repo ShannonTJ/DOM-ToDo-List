f7ca20b3b00e1fb23f17525edba63fd090d2c050
Shannon TJ <shannontj@shaw.ca>
2020-11-22 11:12:03 -0700

    file setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
