59a2962a9ab77158206ef9febbf7dbdb28c5ae54
Shannon TJ <shannontj@shaw.ca>
2020-11-23 16:04:04 -0700

    functional project

--- src/App.js
+++ src/App.js
@@ -12,22 +12,40 @@ function App() {
   //filter tasks based on completion
   const filterHandler = () => {
     switch (filter) {
+      case "all":
+        setFilterTasks(tasks);
+        break;
       case "complete":
         setFilterTasks(tasks.filter((task) => task.completed === true));
         break;
       case "incomplete":
         setFilterTasks(tasks.filter((task) => task.completed === false));
         break;
-      default:
-        setFilterTasks(tasks);
-        break;
     }
   };
+  useEffect(() => {
+    getFromLocal();
+  }, []);
 
   //app refreshes when tasks are submitted and the dropdown is changed
   useEffect(() => {
     filterHandler();
+    saveToLocal();
   }, [tasks, filter]);
+
+  //load from local storage
+  const getFromLocal = () => {
+    if (localStorage.getItem("tasks") === null) {
+      localStorage.setItem("tasks", JSON.stringify([]));
+    } else {
+      setTasks(JSON.parse(localStorage.getItem("tasks")));
+    }
+  };
+
+  //save to local storage
+  const saveToLocal = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
 
   return (
     <div className="App">
