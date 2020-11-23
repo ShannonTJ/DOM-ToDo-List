5efd5c844fe3b772bac97e93d36c9a02c82e5488
Shannon TJ <shannontj@shaw.ca>
2020-11-23 12:30:35 -0700

    form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 const Form = () => {
-  return <form></form>;
+  return (
+    <form>
+      <input type="text" className="task-input" />
+      <button className="task-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="tasks" className="filter-task">
+          <option value="all">All</option>
+          <option value="complete">Complete</option>
+          <option value="incomplete">Incomplete</option>
+        </select>
+      </div>
+    </form>
+  );
 };
+
+export default Form;
